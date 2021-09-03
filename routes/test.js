0fd49a0c84c036b3ceb531d6ec49b90103ff8d2c
lightbell03 <bellight28@gmail.com>
2021-09-03 16:07:05 +0900

    test

---
+++ routes/test.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  var userEmail = req.body.email;
+  var password = req.body.password;
+  var test = "hello world";
+  res.write("<h1>"+userEmail+"<h1>");
+});
+
+module.exports = router;
