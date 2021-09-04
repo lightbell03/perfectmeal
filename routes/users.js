b19c5151019908031199f2f7f22145bba17854e9
lightbell03 <bellight28@gmail.com>
2021-09-04 11:53:27 +0900

    test

--- routes/users.js
+++ routes/users.js
@@ -17,31 +17,26 @@ var con = mysql.createConnection({
 //  })
 //);
 
-//router.post('/', function(req, res) {
-//    var userEmail = req.body.email;
-//    var password = req.body.password;
-//
-//    con.query("SELECT * FROM db_test WHERE email = ? AND password = ?", [userEmail, password], function(err, row) {
-//        if(err) {
-//          res.write("test");
-//          res.send({status: 'fail'});
-//          console.log(err);
-//        }
-//
-//        if(row.length > 0){
-//          console.log("success");
-//          res.send({status: 'success', email: userEmail});
-//        }else{
-//          console.log("fail");
-//          //res.send({"success": 'success', "message": "User not found, please try again"});
-//          res.send({status: 'fail'});
-//        }
-//    });
-//});
+router.post('/', function(req, res) {
+    var userEmail = req.body.email;
+    var password = req.body.password;
 
-router.get('/', function(req, res){
-  var test = "test";
-  res.write("<h1>" + test + "</h1>");
-  res.send({status: "success"});
-})
+    con.query("SELECT * FROM db_test WHERE email = ? AND password = ?", [userEmail, password], function(err, row) {
+        if(err) {
+          res.write("test");
+          res.send({status: 'fail'});
+          console.log(err);
+        }
+
+        if(row.length > 0){
+          console.log("success");
+          res.send({status: 'success', email: userEmail});
+        }else{
+          console.log("fail");
+          //res.send({"success": 'success', "message": "User not found, please try again"});
+          res.send({status: 'fail'});
+        }
+    });
+});
+
 module.exports = router;
