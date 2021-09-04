c5a2f0fa76cc50f28bed643de7ff4bb8510318e6
lightbell03 <bellight28@gmail.com>
2021-09-04 13:24:46 +0900

    test

--- routes/users.js
+++ routes/users.js
@@ -3,12 +3,12 @@ const { createProxyMiddleware } = require("http-proxy-middleware");
 var router = express.Router();
 var mysql = require("mysql");
 
-//var con = mysql.createConnection({
-//    host: "localhost",
-//    user: "root",
-//    password: "Lightbell03!",
-//    database: "db_test"
-//});
+var con = mysql.createConnection({
+    host: "us-cdbr-east-04.cleardb.com",
+    user: "b279a5641ab7e3",
+    password: "2c01f496",
+    database: "heroku_828ebb76607f724"
+});
 
 //router.use(
 //  createProxyMiddleware("/users", {
