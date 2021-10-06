1662c77396bb74a15a176da3aa6dc1b807b313aa
lightbell03 <bellight28@gmail.com>
2021-10-06 16:18:54 +0900

    test

--- routes/sqlcon.js
+++ routes/sqlcon.js
@@ -1,16 +1,16 @@
 var mysql = require("mysql");
 
-//var con = mysql.createConnection({
-//    host: "us-cdbr-east-04.cleardb.com",
-//    user: "b279a5641ab7e3", 
-//    password: "2c01f496",
-//    database: "heroku_828ebb76607f724"
-//});
 var con = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "Lightbell03!",
-    database: "db_test",
+    host: "us-cdbr-east-04.cleardb.com",
+    user: "b279a5641ab7e3", 
+    password: "2c01f496",
+    database: "heroku_828ebb76607f724"
 });
+//var con = mysql.createConnection({
+//    host: "localhost",
+//    user: "root",
+//    password: "Lightbell03!",
+//    database: "db_test",
+//});
 
 module.exports = con;
