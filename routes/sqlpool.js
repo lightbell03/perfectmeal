4feb08a0791750fcb14e86aa242108e1f13d14d9
lightbell03 <bellight28@gmail.com>
2021-10-06 16:41:10 +0900

    test

--- routes/sqlpool.js
+++ routes/sqlpool.js
@@ -1,17 +1,17 @@
 const mysql = require('mysql2/promise');
 
-//var pool = mysql.createConnection({
-//    host: "us-cdbr-east-04.cleardb.com",
-//    user: "b279a5641ab7e3", 
-//    password: "2c01f496",
-//    database: "heroku_828ebb76607f724"
-//});
-
-const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    password: 'Lightbell03!',
-    database: 'db_test'
+var pool = mysql.createConnection({
+    host: "us-cdbr-east-04.cleardb.com",
+    user: "b279a5641ab7e3", 
+    password: "2c01f496",
+    database: "heroku_828ebb76607f724"
 });
+
+//const pool = mysql.createPool({
+//    host: 'localhost',
+//    user: 'root',
+//    password: 'Lightbell03!',
+//    database: 'db_test'
+//});
 
 module.exports = pool;
